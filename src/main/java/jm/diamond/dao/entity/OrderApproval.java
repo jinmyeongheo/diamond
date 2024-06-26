48c75d1e770469d925c9fb2a41f16df84943aeb8
jinmyeong.heo <jinmyeong.heo@seeroo.co.kr>
2024-06-26 17:34:28 +0900

    엔티티 추가

--- src/main/java/jm/diamond/dao/entity/OrderApproval.java
+++ src/main/java/jm/diamond/dao/entity/OrderApproval.java
@@ -1,11 +1,8 @@
 package jm.diamond.dao.entity;
 
-import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-import lombok.Setter;
 
 @Entity
 public class OrderApproval {
