ff672b397a8924e42206a5ee375cb11f52137ab2
jinmyeong.heo <jinmyeong.heo@seeroo.co.kr>
2025-09-05 17:42:42 +0900

    .

--- src/test/java/jm/diamond/batch/SimpleChunkJobConfigTest.java
+++ src/test/java/jm/diamond/batch/SimpleChunkJobConfigTest.java
@@ -1,10 +1,15 @@
 package jm.diamond.batch;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -12,8 +17,18 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles("local")
 class SimpleChunkJobConfigTest {
 
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
     @Test
-    void test(){
+    void test() throws Exception {
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+        // then
+        assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+
         System.out.println("true = " + true);
     }
 }
