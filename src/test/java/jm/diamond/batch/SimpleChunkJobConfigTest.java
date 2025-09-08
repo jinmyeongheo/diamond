c4d24dcebb24838de1898fcf44169072a9527764
jinmyeong.heo <jinmyeong.heo@seeroo.co.kr>
2025-09-09 01:55:15 +0900

    테스트

--- src/test/java/jm/diamond/batch/SimpleChunkJobConfigTest.java
+++ src/test/java/jm/diamond/batch/SimpleChunkJobConfigTest.java
@@ -11,7 +11,9 @@ import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,12 +28,23 @@ class SimpleChunkJobConfigTest {
 //    private JobLauncherTestUtils jobLauncherTestUtils;
 
     @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
     private OrderInfoRepository orderInfoRepository;
 
     @Test
     void JDBC_커넥션_테스트(){
         List<OrderInfo> all =
             orderInfoRepository.findAll();
+    }
+
+    @Test
+    void checkTransactionManager() { // JpaTransactionManager는 같은 DataSource를 쓰는 plain JDBC 접근(= MyBatis/JdbcTemplate 등)을 동일 트랜잭션에 참여시킬 수 있습니다
+        PlatformTransactionManager txManager =
+            applicationContext.getBean(PlatformTransactionManager.class);
+
+        System.out.println(">>> TransactionManager class = " + txManager.getClass().getName());
     }
 
 //    @Test
